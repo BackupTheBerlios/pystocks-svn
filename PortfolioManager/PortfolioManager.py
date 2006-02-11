8ef61eadee0d287153120ae6ae74fc569b167f24
nicolascouture <nicolascouture@cdc6e4ff-b307-0410-9412-9d50049c1cb6>
2006-02-11 18:28:24 +0000

    Added stuff to portfolio manager prototype

--- PortfolioManager/PortfolioManager.py
+++ PortfolioManager/PortfolioManager.py
@@ -1,27 +1,55 @@
 #!/usr/bin/python
 #
 
+import os
+import cPickle
+
+from pystocks.YahooFinance import (YahooQuoteFinder,
+                                   SymbolError,
+                                   FeedError,
+                                   format_number)
+
 class PortfolioManager:
     def __init__(self, name, feed):
         """
-        Load or Create a named portfolio.
+        Initialize a portfolio object.
         
         name: portfolio name
         feed: pystocks feed callable
         """
         pass
 
+    def create(self):
+        """
+        Create portfolio.
+        """
+        # if portfolio exists:
+        #     raise error.
+        # otherwise:
+        #     create it.
+        pass
+
     def add(self, symbol, amount, paid_price):
         """
-        Add stocks to your portfolio.
+        Add stocks to portfolio.
 
         symbol: stock's symbol
         amount: amount of shares
         price:  paid price
         """
+        self._exist_or_die()
         pass
 
     def remove(self, symbol):
         """
-        """
+        Remove stocks from portfolio.
 
+        symbol: stock's symbol
+        """
+        self._exist_or_die()
+        pass
+
+    def _exist_or_die(self):
+        # if portfolio doesn't exist:
+        #     raise error
+        pass
